92b6a62cd70cb599ff21f3f41a504b4cae539441
Thomas Willson <thomas.willson@me.com>
2016-04-16 10:09:08 -0700

    More reliable CPFERotarySwitch code.

--- CPFERotarySwitch.h
+++ CPFERotarySwitch.h
@@ -1,3 +1,6 @@
+#include <avr/interrupt.h>
+#include <util/atomic.h>
+
 #include <AVRLibrary/arduino/Arduino.h>
 #include <AVRLibrary/CPFEAVRAnalog.h>
 
@@ -5,7 +8,7 @@ void rotarySwitchResultHandler(uint16_t result, void *info);
 
 class CPFERotarySwitch {
 public:
-	static const uint8_t NUM_ROTARYS = 3;
+	static constexpr uint8_t NUM_ROTARYS = 3;
 
 	enum class RotarySwitches
 		: uint8_t {
@@ -14,22 +17,20 @@ public:
 private:
 
 	static uint8_t positionCount;
-	static int8_t currentPositionRetrieval;
-	static bool pendingRetrievalRequest;
+	static volatile int8_t currentRotaryADCConversion;
 
-	static const uint8_t RED_ADC = 1;
-	static const uint8_t YELLOW_ADC = 0;
-	static const uint8_t BLACK_ADC = 2;
+	static constexpr uint8_t RED_ADC = 1;
+	static constexpr uint8_t YELLOW_ADC = 0;
+	static constexpr uint8_t BLACK_ADC = 2;
 
 	static CPFEAVRAnalog analogPins[NUM_ROTARYS];
-	static uint8_t positions[NUM_ROTARYS];
+	static volatile uint8_t positions[NUM_ROTARYS];
 
 public:
 
 	static void init(uint8_t positionCount) {
 		CPFERotarySwitch::positionCount = positionCount;
-		pendingRetrievalRequest = false;
-		currentPositionRetrieval = -1;
+		currentRotaryADCConversion = -1;
 
 		for (int i = 0; i < NUM_ROTARYS; ++i) {
 			analogPins[i].analogPinNumber = i;
@@ -37,9 +38,10 @@ public:
 	}
 
 	static void requestUpdatedPositions() {
-		if (currentPositionRetrieval < 0) {
-			currentPositionRetrieval = 0;
-			pendingRetrievalRequest = true;
+		ATOMIC_BLOCK(ATOMIC_FORCEON) {
+			if (currentRotaryADCConversion < 0) { // If previous set of conversions finished, start a new round
+				currentRotaryADCConversion = 0;
+			}
 		}
 	}
 
@@ -47,33 +49,32 @@ public:
 		return positions[(uint8_t) rotarySwitch];
 	}
 
-	static void runTasks() {
-		if (pendingRetrievalRequest) {
-			while (!CPFEAVRAnalog::adcAvailable())
+	static void runTasks() { // Start any requested conversions.
+		if (currentRotaryADCConversion >= 0) {
+			while (!CPFEAVRAnalog::adcAvailable()) // Wait for conversion in progress to finish
 				;
-
-			analogPins[currentPositionRetrieval].startConversion(&rotarySwitchResultHandler, nullptr);
+			analogPins[currentRotaryADCConversion].startConversion(&rotarySwitchResultHandler, nullptr);
 		}
 	}
 
+	static uint8_t calculatePositionFromADCResult(uint16_t result) {
+		uint8_t position = (uint8_t) (positionCount - (float) result / (1024.0 / (float) positionCount));
+
+		if (position > positionCount - 1) {
+			position = positionCount - 1;
+		}
+
+		return position;
+	}
+
 	static void resultHandler(uint16_t result, void *info) {
-		Serial.printf("%d, %d\n", result, currentPositionRetrieval);
+		if (currentRotaryADCConversion >= 0) {
+			positions[currentRotaryADCConversion] = calculatePositionFromADCResult(result);
 
-		if (currentPositionRetrieval >= 0) {
-			positions[currentPositionRetrieval] = (uint8_t)(positionCount - (float)result / (1024.0 / (float)positionCount));
-
-			if (positions[currentPositionRetrieval] > 11) {
-				positions[currentPositionRetrieval] = 11;
-			}
-
-			Serial.printf("%d\n", positions[currentPositionRetrieval]);
-
-			if (currentPositionRetrieval < NUM_ROTARYS - 1) {
-				++currentPositionRetrieval;
-				pendingRetrievalRequest = true;
+			if (currentRotaryADCConversion < NUM_ROTARYS - 1) {
+				++currentRotaryADCConversion;
 			} else {
-				currentPositionRetrieval = 0;
-				pendingRetrievalRequest = true;
+				currentRotaryADCConversion = -1;
 			}
 		}
 	}
